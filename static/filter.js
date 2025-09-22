84b8d8371e18c11b9a7720b9c3722ceea21e9678
ell1e <el@horse64.org>
2025-09-22 14:42:00 +0200

    Add JavaScript handling to actually show the tag descriptions

--- static/filter.js
+++ static/filter.js
@@ -1,24 +1,31 @@
-// Filters the projects by tags defined in projects.yml
-// Implemented by Matt Hall (github.com/mh15)
+// Filters the projects by tags defined in projects.yml and
+// shows tag descriptions.
+// Implemented by Matt Hall (github.com/mh15), modified by others.
 
 const projects = document.querySelector(".projects-list")
 const filters = new Set()
 
-// Only enable the feature if JS is enabled
 // Add the tag buttons in JS
 const tagContainer = document.querySelector("#tags")
-if (tagContainer != null) {
-    const tagNames = getTags(tagContainer, "data-all-tags")
-    if (tagNames.length > 0) {
-        tagNames.forEach(name => {
-            let tag = document.createElement("div")
-            tag.classList.add("shell", "tag")
-            tag.innerHTML = name
-            tag.onclick = () => {
-                setFilter(tag, name)
-            }
-            tagContainer.appendChild(tag)
-        })
+let tags = new Object()
+
+/**
+ * This loads up the tags and adds the tag buttons
+ */
+function addTagButtons() {
+    if (tagContainer == null) {
+        return;
+    }
+    tags = getTags(tagContainer, null, false)
+    console.log("filter.js: Tags data was loaded: " + JSON.stringify(tags))
+    for (const entry of Object.values(tags)) {
+        let tag = document.createElement("div")
+        tag.classList.add("shell", "tag")
+        tag.innerHTML = entry.name
+        tag.onclick = () => {
+            setFilter(tag, entry)
+        }
+        tagContainer.appendChild(tag)
     }
 }
 
@@ -29,17 +36,17 @@ if (tagContainer != null) {
  * @returns {number}
  */
 function projectComparator(a, b) {
-    const tagsA = getTags(a, "data-tags")
-    const tagsB = getTags(b, "data-tags")
+    const tagsA = getTags(a, "data-tags", true)
+    const tagsB = getTags(b, "data-tags", true)
 
     // Get a score for the projects
     let scoreA = 0
     let scoreB = 0
-    for (const tag of filters) {
-        if (tagsA.includes(tag)) {
+    for (const tagName of filters) {
+        if (tagsA.includes(tagName)) {
             scoreA++
         }
-        if (tagsB.includes(tag)) {
+        if (tagsB.includes(tagName)) {
             scoreB++
         }
     }
@@ -48,7 +55,6 @@ function projectComparator(a, b) {
     if (scoreB != filters.size) scoreB = 0
 
     if (filters.size > 0) {
-
         if (scoreA == 0) a.classList.add("project-filtered")
         if (scoreB == 0) b.classList.add("project-filtered")
     }
@@ -64,16 +70,48 @@ function projectComparator(a, b) {
 }
 
 /**
- * Set the current filters and sort
+ * For some reason, JS doesn't have a HTML escape built-in.
+ * @param {string} s
+ */
+function escapeHTML(s) {
+    return s.replaceAll("<", "&lt;").
+        replaceAll(">", "&gt;").
+        replaceAll("&", "&amp;").
+        replaceAll("'", "&#039;").
+        replaceAll('"', "&quot;")
+}
+
+/**
+ * Set the current filters and sort.
  * @param {HTMLElement} el 
- * @param {string|null} f 
+ * @param {object|null} f 
  */
 function setFilter(el, f) {
-    if (f == null) {
+    let tagName = null
+    if (f != null) {
+        tagName = f.name + ""
+    }
+    console.log("filter.js: Clicked tag: " + tagName)
+    let tagSummaryDiv = document.querySelector(".tags-summary-field")
+    if (tagSummaryDiv == null) {
+        tagSummaryDiv = document.createElement("div")
+        tagSummaryDiv.classList.add("tags-summary-field")
+        tagSummaryDiv.innerHTML = ""
+        tagSummaryDiv.style.display = "none"
+        tagContainer.appendChild(tagSummaryDiv)
+    }
+
+    if (tagName == null || (filters.length == 1 &&
+            filters.has(tagName))) {
         // Turn off all filters
         document.querySelectorAll(".tag").forEach(tag => {
             tag.classList.remove("tag-selected")
         })
+        console.log("filter.js: Filters are being cleared.")
+
+        // Hide the tag summary:
+        tagSummaryDiv.innerHTML = ""
+        tagSummaryDiv.style.display = "none"
 
         filters.clear()
     } else {
@@ -81,10 +119,32 @@ function setFilter(el, f) {
         el.classList.toggle("tag-selected")
 
         // Add or remove from the list of filters
-        if (filters.has(f)) {
-            filters.delete(f)
+        if (filters.has(tagName)) {
+            filters.delete(tagName + "")
         } else {
-            filters.add(f)
+            filters.add(tagName + "")
+        }
+        console.log("filter.js: Filters were changed to: " +
+            JSON.stringify(Array.from(filters)))
+
+        // Display the tag description(s):
+        let tagDescHtml = ""
+        for (const filterItem of filters) {
+            let desc = tags[filterItem]["description"]
+            if (desc === null || desc == "") {
+                continue
+            }
+            tagDescHtml += "<tag-desc><em>" + escapeHTML(filterItem) +
+                ":</em> " + escapeHTML(desc) + "</tag-desc>"
+        }
+        if (tagDescHtml != "") {
+            tagDescHtml = "<h4>Info about selected categories:</h4>" +
+                tagDescHtml
+            tagSummaryDiv.innerHTML = tagDescHtml
+            tagSummaryDiv.style.display = "block"
+        } else {
+            tagSummaryDiv.innerHTML = ""
+            tagSummaryDiv.style.display = "none"
         }
     }
     Array.from(projects.children).forEach(project => {
@@ -110,13 +170,40 @@ function setFilter(el, f) {
 /**
  * Get the tags for a project
  * @param {HTMLElement} el
- * @param {string} attribute
+ * @param {string|null) attribute
  * @returns {string[]}
  */
-function getTags(el, attribute) {
+function getTags(el, attribute, asNames) {
+    if (asNames !== true) {
+        asNames = false
+    }
+    if (attribute == null) {
+        attribute = "data-tags-all-names"
+    }
     const data = el.getAttribute(attribute)
     if (!data) return []
-    const list = data.split(",")
+    const namelist = data.split(",")
+    if (asNames) {
+        return namelist
+    }
 
-    return list
+    let result = {}
+    for (const name of namelist) {
+        if (name.trim() == "") {
+            continue
+        }
+        let desc = el.getAttribute("data-tag-desc-" +
+            name.replace(" ", "-"))
+        if (desc != null && desc.trim() == "") {
+            desc = null
+        }
+        result[name] = {
+            "name": name,
+            "description": desc,
+        }
+    }
+    return result
 }
+
+addTagButtons()
+
